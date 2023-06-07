af4509da35fce3ac5a8d5d571d245d831ae0071d
thouger <1030490158@qq.com>
2023-06-07 23:52:49 +0800

    添加hooklibc函数

---
+++ so/libc_method.js
@@ -0,0 +1,32 @@
+// Hook libc.so
+var mkdirat_addr = null;
+
+var symbols = Process.findModuleByName("libc.so").enumerateSymbols();
+for (var i = 0; i < symbols.length; i++) {
+  if (symbols[i].name === "mkdirat") {
+    mkdirat_addr = symbols[i].address;
+    break;
+  }
+}
+
+if (mkdirat_addr) {
+  var output = "";
+
+  Interceptor.attach(mkdirat_addr, {
+    onEnter: function (args) {
+      var dirfd = args[0];
+      var pathname = Memory.readUtf8String(args[1]);
+      var mode = args[2];
+      output = "mkdirat() called with dirfd: " + dirfd + ", pathname: " + pathname + ", mode: " + mode;
+      output += "\nStack trace:\n" +
+        Thread.backtrace(this.context, Backtracer.ACCURATE)
+          .map(DebugSymbol.fromAddress).join('\n');
+    },
+    onLeave: function (retval) {
+      console.log(output+"\nReturn value: "+retval+"\n\n\n");
+      // You can add additional code here if needed
+    }
+  });
+} else {
+  console.log("Failed to find the address of mkdirat");
+}
